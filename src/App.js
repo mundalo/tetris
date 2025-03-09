f3c3bd41b897d6eb8c4d2d6e6dc7d724c0abb4cc
Mundal1994 <manga_flip@hotmail.com>
2025-03-09 21:40:01 +0200

    initial creation of the tetris react app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+//import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
+/*
+<a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+*/
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello world
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
